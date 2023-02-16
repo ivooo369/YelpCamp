32d09144334d37a2ed94240711eada0a5d070c83
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-02-16 21:30:35 +0200

    Make changes on image uploading

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,20 @@ const seedDB = async () => {
             author: '63e55df637d1fab58a540dd1',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dprh57lps/image/upload/v1676565963/YelpCamp/qivbrkh4g0ewvawkspca.jpg',
+                    filename: 'YelpCamp/qivbrkh4g0ewvawkspca'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dprh57lps/image/upload/v1676565962/YelpCamp/rrjuem3scld4lffbz33p.jpg',
+                    filename: 'YelpCamp/rrjuem3scld4lffbz33p'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dprh57lps/image/upload/v1676565963/YelpCamp/rwbrnbzhh0nuui4teysv.jpg',
+                    filename: 'YelpCamp/rwbrnbzhh0nuui4teysv'
+                }
+            ],
             description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, numquam tenetur ipsa aperiam, atque saepe beatae deserunt repudiandae minus assumenda quidem distinctio aliquam repellat itaque.',
             price
         });
